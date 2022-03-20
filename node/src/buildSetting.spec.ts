a854386ee1ad285dc278950ca9aabac802069b33
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2022-03-21 01:54:30 +0900

    add buildSetting test

--- node/src/buildSetting.spec.ts
+++ node/src/buildSetting.spec.ts
@@ -1,31 +1,31 @@
 import * as BuildSetting from "./buildSetting";
+import * as Config from "./config";
 
-jest.mock("./config", () => {
-  return {
-    getConfig: jest
-      .fn()
-      .mockImplementationOnce(() => {
-        return {
-          tabs: true,
-        };
-      })
-      .mockImplementationOnce(() => {
-        return {
-          tabs: false,
-        };
-      }),
-  };
-});
+jest.mock("./config");
+
+const getConfigMock = Config.getConfig as jest.Mock;
 
 describe("buildsetting", () => {
   describe("getSpaceChar", () => {
     it("when tabs is true, then return tab character", () => {
+      getConfigMock.mockImplementation(() => {
+        return {
+          tabs: true,
+        };
+      });
+
       const result = BuildSetting.getSpaceChar();
 
       expect(result).toBe("\t");
     });
 
     it("when tabs is false, then return shitespace character", () => {
+      getConfigMock.mockImplementation(() => {
+        return {
+          tabs: false,
+        };
+      });
+
       const result = BuildSetting.getSpaceChar();
 
       expect(result).toEqual(" ");
